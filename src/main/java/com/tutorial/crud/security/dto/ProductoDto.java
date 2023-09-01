df43a6b7136c98fefeb7a98d6f212d5d5ea0d111
lucas <robledolucas42@gmail.com>
2023-08-31 21:44:33 -0300

    ProductoDto hecho

--- src/main/java/com/tutorial/crud/security/dto/ProductoDto.java
+++ src/main/java/com/tutorial/crud/security/dto/ProductoDto.java
@@ -1,5 +1,24 @@
 package com.tutorial.crud.security.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProductoDto {
+    @NotBlank
+    private String nombre;
+    @Min(0)
+    private Float precio;
+
+    public ProductoDto(@NotBlank String nombre, @Min(0) Float precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
     
 }
